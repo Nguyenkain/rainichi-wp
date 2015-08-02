1db4e1f8ef546d4c0fde5509f4b9bcd2f30ec4d2
hugnnm <hungnm@newwave.vn>
2015-08-03 00:07:16 +0700

    tin tuc

--- wp-content/themes/rainichi/single.php
+++ wp-content/themes/rainichi/single.php
@@ -9,40 +9,104 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="container common">
+	<!-- Example row of columns -->
+	<div class="row">
+		<div class="col-md-3 sidebar">
+			<?php get_sidebar() ?>
+		</div>
+		<div class="col-md-9 content">
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post();
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+				/*
+				 * Include the post format-specific template for the content. If you want to
+				 * use this in a child theme, then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				?>
+				<div class="row news-item">
+					<div class="col-xs-2 news-item-date">
+						<?php $date = date('Y-m-d',strtotime(get_the_date()));?>
+						<div><?php echo date('d/m',strtotime($date)); ?><br/><?php echo date('Y',strtotime($date)); ?></div>
+					</div>
+					<div class="col-xs-10 news-item-info">
+						<h4><?php the_title() ?></h4>
+						<?php the_content() ?>
+						<div class="row options">
+							<div class="col-xs-12 col-sm-9 link-map">
+								<ul>
+									<li><a href="<?php the_permalink() ?>"><?php comments_number() ?></a></li>
+									<li><a href="<?php comment_link()?>"><?php if(has_tag()) { the_tags(); } else { echo 'Tags:'; }?></a></li>
+									<li><a href="#">Share</a></li>
+								</ul>
+							</div>
+							<div class="col-xs-12 col-sm-3 sns">
+								<ul>
+									<li><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/sns-1-hover.png" class="img-responsive"></a></li>
+									<li><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/sns-2-hover.png" class="img-responsive"></a></li>
+									<li><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/sns-3-hover.png" class="img-responsive"></a></li>
+									<li><a href="#"><img src="<?php bloginfo( 'template_url' ); ?>/img/sns-4-hover.png" class="img-responsive"></a></li>
+								</ul>
+							</div>
+						</div>
 
-			/*
-			 * Include the post format-specific template for the content. If you want to
-			 * use this in a child theme, then include a file called called content-___.php
-			 * (where ___ is the post format) and that will be used instead.
-			 */
-			get_template_part( 'content', get_post_format() );
+						<div class="row comment">
+							<textarea class="form-control" placeholder="Ý kiến của bạn"></textarea>
+							<div class="submit-btn">
+								<button type="button">Gửi</button>
+							</div>
+						</div>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+						<div class="row more-news">
+							<?php
+							$related = get_posts( array( 'category__in' => wp_get_post_categories(get_the_ID()), 'numberposts' => 4, 'post__not_in' => array(get_the_ID()) ) );
+							if( $related ) foreach( $related as $post ) {
+								setup_postdata($post); ?>
+								<div class="col-xs-6 col-sm-3 more-news-item">
+									<div class="date"><?php $date = date('Y-m-d',strtotime(get_the_date()));?>
+										<?php echo date('d/m',strtotime($date)); ?><br/><?php echo date('Y',strtotime($date)); ?></div>
+									<div class="news-title"><a href="<?php the_permalink()?>"><span><?php the_title()?></span></a></div>
+								</div>
+							<?php }
+							wp_reset_postdata(); ?>
+						</div>
 
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
+						<div class="row related-news">
+							<ul>
+								<?php
+								$related = get_posts( array( 'category__in' => wp_get_post_categories(get_the_ID()), 'numberposts' => 4, 'post__not_in' => array(get_the_ID()) ) );
+								if( $related ) foreach( $related as $post ) {
+									setup_postdata($post); ?>
+									<li><a href="<?php the_permalink()?>"><?php the_title()?></a></li>
+								<?php }
+								wp_reset_postdata(); ?>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<?php
+//				get_template_part( 'content', get_post_format() );
 
-		// End the loop.
-		endwhile;
-		?>
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+//					comments_template();
+				endif;
 
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+				// End the loop.
+			endwhile;
+			?>
+
+			<div class="see-more">
+				<a href="#">&gt;&gt; Xem thêm</a>
+			</div>
+			<div class="bottom-triangle">
+				<img src="<?php bloginfo( 'template_url' ); ?>/img/bottom-triangle.png" class="img-responsive">
+			</div>
+		</div>
+	</div>
+	<hr>
+</div>
 
 <?php get_footer(); ?>
