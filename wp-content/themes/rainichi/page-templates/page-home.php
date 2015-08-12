48f124d3a47ce234cf09b61403b53e5b3aac350f
Nguyenkain <Nguyenkain@gmail.com>
2015-08-13 00:26:01 +0700

    news cho trang chủ

--- wp-content/themes/rainichi/page-templates/page-home.php
+++ wp-content/themes/rainichi/page-templates/page-home.php
@@ -8,15 +8,24 @@
 get_header(); ?>
 
 	<div class="container">
-		<div class="row reason"><img class="img-responsive" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-top.png" alt="" /></div>
+		<div class="row reason"><img class="img-responsive"
+		                             src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-top.png" alt=""/>
+		</div>
 		<div class="row reason-list">
 			<div class="col-xs-6 col-sm-3 reason-item">
 				<div class="flip-container">
 					<div class="flipper">
 
-						<img class="img-responsive" style="width: 100%; visibility: hidden;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1.png" alt="" data-holder-rendered="true" />
-						<div class="front"><img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1.png" alt="" data-holder-rendered="true" /></div>
-						<div class="back"><img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1-hover.png" alt="" data-holder-rendered="true" /></div>
+						<img class="img-responsive" style="width: 100%; visibility: hidden;"
+						     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1.png" alt=""
+						     data-holder-rendered="true"/>
+
+						<div class="front"><img class="img-responsive" style="width: 100%; display: block;"
+						                        src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1.png"
+						                        alt="" data-holder-rendered="true"/></div>
+						<div class="back"><img class="img-responsive" style="width: 100%; display: block;"
+						                       src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-1-hover.png"
+						                       alt="" data-holder-rendered="true"/></div>
 					</div>
 				</div>
 				<div class="caption">
@@ -24,19 +33,28 @@ get_header(); ?>
 				</div>
 			</div>
 			<div class="col-xs-6 col-sm-3 reason-item"><a class="thumbnail" href="#">
-					<img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-2.png" alt="" data-holder-rendered="true" /></a>
+					<img class="img-responsive" style="width: 100%; display: block;"
+					     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-2.png" alt=""
+					     data-holder-rendered="true"/></a>
+
 				<div class="caption">
 					<h5>TIẾT KIỆM NHẤT</h5>
 				</div>
 			</div>
 			<div class="col-xs-6 col-sm-3 reason-item"><a class="thumbnail" href="#">
-					<img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-3.png" alt="" data-holder-rendered="true" /></a>
+					<img class="img-responsive" style="width: 100%; display: block;"
+					     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-3.png" alt=""
+					     data-holder-rendered="true"/></a>
+
 				<div class="caption">
 					<h5>THOẢI MÁI NHẤT</h5>
 				</div>
 			</div>
 			<div class="col-xs-6 col-sm-3 reason-item"><a class="thumbnail" href="#">
-					<img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-4.png" alt="" data-holder-rendered="true" /></a>
+					<img class="img-responsive" style="width: 100%; display: block;"
+					     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/4-4.png" alt=""
+					     data-holder-rendered="true"/></a>
+
 				<div class="caption">
 					<h5>HIỆU QUẢ NHẤT</h5>
 				</div>
@@ -45,7 +63,8 @@ get_header(); ?>
 		<div class="row introduce-area">
 			<div class="col-md-5 introduce">
 				<div>
-					<div class="intro-head"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-cam.png" alt="" />
+					<div class="intro-head"><img
+							src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-cam.png" alt=""/>
 						<span class="intro-head-title">RAN</span></div>
 					<div class="intro-body">
 						<h5>YOKEN</h5>
@@ -55,38 +74,49 @@ get_header(); ?>
 						nghiệm giảng dạy và phương pháp hiệu quả.
 
 					</div>
-					<div class="intro-foot"><a href="http://rainichi.los.vn/khoa-hoc/yoken/">Chi tiết khoá học &gt;&gt;</a></div>
+					<div class="intro-foot"><a href="http://rainichi.los.vn/khoa-hoc/yoken/">Chi tiết khoá học
+							&gt;&gt;</a></div>
 				</div>
 			</div>
 			<div class="col-md-7 course">
-				<div class="course-title"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten.png" alt="" />
-					CÁC KHOÁ HỌC MỚI</div>
+				<div class="course-title"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten.png"
+				                               alt=""/>
+					CÁC KHOÁ HỌC MỚI
+				</div>
 				<div class="day">
 
 					28
-					<div class="description">Thứ <strong>hai</strong> - Thứ <strong>sáu</strong>, cấp độ <strong>N4</strong>
+					<div class="description">Thứ <strong>hai</strong> - Thứ <strong>sáu</strong>, cấp độ
+						<strong>N4</strong>
 						Sáng <strong>từ 8:00</strong>, chiều <strong>từ 13:30</strong></div>
 				</div>
 				<div class="info-course clearfix">
 					<div class="item-course big-item"><a href="#">Đăng ký ngay</a></div>
 					<div class="item-course"><a href="#">Tháng 6</a>
-						<img class="img-responsive" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/left-arrow.png" alt="left" /></div>
+						<img class="img-responsive"
+						     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/left-arrow.png" alt="left"/>
+					</div>
 					<div class="item-course other-side big-item"><a href="#">Lịch khai giảng</a></div>
 					<div class="item-course other-side"><a href="#">2015</a>
-						<img class="img-responsive" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/right-arrow.png" alt="right" /></div>
+						<img class="img-responsive"
+						     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/right-arrow.png" alt="right"/>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="row title">
-			<div class="common-title"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-cam.png" alt="" />
+			<div class="common-title"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-cam.png"
+			                               alt=""/>
 				<span class="intro-head-title">HÃY GỌI CHO CHÚNG TÔI</span>
 				<span class="telephone-num">09000000888</span></div>
 		</div>
 		<div class="row contact">
 			<div class="col-xs-6 col-md-4 contact-item"><a class="thumbnail" href="#">
-					<img class="img-responsive" style="width: 100%; display: block;" src="http://rainichi.los.vn/wp-content/themes/rainichi/img/hoc-thu.png" alt="" data-holder-rendered="true" />
+					<img class="img-responsive" style="width: 100%; display: block;"
+					     src="http://rainichi.los.vn/wp-content/themes/rainichi/img/hoc-thu.png" alt=""
+					     data-holder-rendered="true"/>
 				</a></div>
-			<?php echo do_shortcode('[contact-form-7 id="62" title=""]') ?>
+			<?php echo do_shortcode( '[contact-form-7 id="62" title=""]' ) ?>
 		</div>
 		<div class="row introduce-2-area">
 			<div class="col-md-5 introduce-2">
@@ -99,69 +129,39 @@ get_header(); ?>
 						<p style="text-align: right;">&gt;&gt;</p>
 
 					</div>
-					<div class="col-md-6 col-sm-4"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/the-man.png" alt="" /></div>
+					<div class="col-md-6 col-sm-4"><img
+							src="http://rainichi.los.vn/wp-content/themes/rainichi/img/the-man.png" alt=""/></div>
 				</div>
 			</div>
 			<div class="col-md-7 information">
 				<h5 class="information-head">TIN TỨC</h5>
+
 				<div class="swiper-container">
 					<div class="swiper-wrapper">
-						<div class="swiper-slide">
+						<?php
+						$args    = array( 'numberposts' => 10 );
+						$myposts = get_posts( $args );
+						foreach ( $myposts as $i => $post ) : setup_postdata( $post ); ?>
+							<?php if ( $i % 2 == 0 ) { ?>
+								<div class="swiper-slide">
+							<?php } ?>
 							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
+								<div class="information-item-header">
+									<img src="<?php echo get_bloginfo('template_url') ?>/img/mui-ten-vang.png">
+									<span><?php the_title() ?></span>
+								</div>
+								<div class="information-item-body">
+									<span><?php the_excerpt() ?></span>
+								</div>
+								<div class="information-item-footer">
+									<a href="<?php echo get_the_permalink() ?>"><span>Đọc thêm ></span></a>
+								</div>
 							</div>
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-						</div>
-						<div class="swiper-slide">
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-						</div>
-						<div class="swiper-slide">
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-						</div>
-						<div class="swiper-slide">
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-							<div class="information-item">
-								<div class="information-item-header"><img src="http://rainichi.los.vn/wp-content/themes/rainichi/img/mui-ten-vang.png" alt="" />
-									Rai-nichi khai giảng lớp học mới</div>
-								<div class="information-item-body">Đồng nghiệp của bạn có hoà hợp với nhau?Bạn có muốn làm không khí nơi làm việc trở nên vui vẻ hơn?Đôi dòng chia sẻ dưới đây có thể giúp bạn tìm được câu trả lời!</div>
-								<div class="information-item-footer"><a>Đọc thêm &gt;</a></div>
-							</div>
-						</div>
+							<?php if ( $i % 2 == 1 ) { ?>
+								</div>
+							<?php } ?>
+						<?php endforeach;
+						wp_reset_postdata(); ?>
 					</div>
 					<div class="info-pagination"></div>
 				</div>
